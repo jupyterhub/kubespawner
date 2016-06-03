8c4af64413a34f9cd47053a4994f2e4773a4f6ac
Eli Bixby <elibixby@google.com>
2016-06-02 17:15:04 -0700

    Add explit dependencies to packaging

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 setup(
     name='kubespawner',
     version='0.1',
+    install_requires=[
+        'requests-futures>=0.9.7',
+        'jupyterhub>=0.4.0',
+    ],
     description='JupyterHub Spawner targetting Kubernetes',
     url='http://github.com/yuvipanda/jupyterhub-kubernetes-spawner',
     author='Yuvi Panda',
