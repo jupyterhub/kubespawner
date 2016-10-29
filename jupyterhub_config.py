89a150c0d7929321344c467686eb3c0b9f46ef97
YuviPanda <yuvipanda@gmail.com>
2016-10-29 13:16:31 -0700

    Add more setup instructions

---
+++ jupyterhub_config.py
@@ -0,0 +1,20 @@
+c.JupyterHub.spawner_class = 'kubespawner.KubeSpawner'
+
+c.JupyterHub.ip = '0.0.0.0'
+c.JupyterHub.hub_ip = '0.0.0.0'
+
+# Don't try to cleanup servers on exit - since in general for k8s, we want
+# the hub to be able to restart without losing user containers
+c.JupyterHub.cleanup_servers = False
+
+# First pulls can be really slow, so let's give it a big timeout
+c.KubeSpawner.start_timeout = 60 * 5 
+
+# Our simplest user image! Optimized to just... start, and be small!
+c.KubeSpawner.singleuser_image_spec = 'yuvipanda/simple-singleuser:v1'
+
+# The spawned containers need to be able to talk to the hub through the proxy!
+c.KubeSpawner.hub_connect_ip = '192.168.61.1'
+
+# Do not use any authentication at all - any username / password will work.
+c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
