fd6a802c0c078e0ef8f6cebfe389416ec230aabd
YuviPanda <yuvipanda@gmail.com>
2016-12-30 16:06:25 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jupyterhub-kubespawner',
-    version='0.5',
+    version='0.5.1',
     install_requires=[
         'jupyterhub',
         'pyyaml',
