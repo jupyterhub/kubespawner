4050f1e55ef541371a61796acd237d3552c1ae6f
yuvipanda <yuvipanda@gmail.com>
2017-05-19 16:07:14 -0700

    Turn off limits in default test config
    Will make it fail with minikube often

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -11,17 +11,13 @@ c.JupyterHub.hub_ip = '0.0.0.0'
 c.JupyterHub.cleanup_servers = False
 
 # First pulls can be really slow, so let's give it a big timeout
-c.KubeSpawner.start_timeout = 60 * 5 
+c.KubeSpawner.start_timeout = 60 * 5
 
 # Our simplest user image! Optimized to just... start, and be small!
 c.KubeSpawner.singleuser_image_spec = 'yuvipanda/simple-singleuser:v1'
 
 # The spawned containers need to be able to talk to the hub through the proxy!
 c.KubeSpawner.hub_connect_ip = os.environ['HUB_CONNECT_IP']
-
-c.KubeSpawner.mem_limit = '2G'
-
-c.KubeSpawner.cpu_limit = 1
 
 # Do not use any authentication at all - any username / password will work.
 c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
