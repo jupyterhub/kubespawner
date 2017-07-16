d62eac151a314de625235004f1a65066bd0c4dab
yuvipanda <yuvipanda@gmail.com>
2017-07-15 23:26:33 -0700

    Refactor PodReflector to make most of the logic generic
    Could be used to add a proxy implementation!

--- kubespawner/reflector.py
+++ kubespawner/reflector.py
@@ -6,15 +6,17 @@ from traitlets import Any, Dict, Unicode
 from kubernetes import client, config, watch
 from tornado.ioloop import IOLoop
 
-class PodReflector(SingletonConfigurable):
+class NamespacedResourceReflector(SingletonConfigurable):
     """
-    Local up-to-date copy of a set of kubernetes pods
+    Local up-to-date copy of a set of kubernetes resources.
+
+    Must be subclassed once per kind of resource that needs watching.
+
+    Note: This design assumes you only want to watch resources in one
+    namespace per application (since this is a singleton)
     """
     labels = Dict(
-        {
-            'heritage': 'jupyterhub',
-            'component': 'singleuser-server',
-        },
+        {},
         config=True,
         help="""
         Labels to reflect onto local cache
@@ -29,12 +31,26 @@ class PodReflector(SingletonConfigurable):
         """
     )
 
-    pods = Dict(
+    resources = Dict(
         {},
         help="""
-        Dictionary of pod names to Pod objects.
+        Dictionary of resource names to the appropriate resource objects.
 
-        This can be directly accessed from multiple threads.
+        This can be accessed across threads safely.
+        """
+    )
+
+    list_method_name = Unicode(
+        None,
+        allow_none=True,
+        help="""
+        Name of function (on a core v1 object) that is to be called to list resources.
+
+        This will be passed a namespace & a label selector. You most likely want something
+        of the form list_namespaced_<resource> - for example, `list_namespaced_pod` will
+        give you a PodReflector.
+
+        This must be set by a subclass.
         """
     )
 
@@ -57,27 +73,27 @@ class PodReflector(SingletonConfigurable):
 
     def _list_and_update(self):
         """
-        Update current list of pods by doing a full fetch of pod information.
+        Update current list of resources by doing a full fetch.
 
-        Overwrites all current pod info.
+        Overwrites all current resource info.
         """
-        initial_pods = self.api.list_namespaced_pod(
+        initial_resources = getattr(self.api, self.list_method_name)(
             self.namespace,
             label_selector=self.label_selector
         )
         # This is an atomic operation on the dictionary!
-        self.pods = {p.metadata.name: p for p in initial_pods.items}
+        self.resources = {p.metadata.name: p for p in initial_resources.items}
         # return the resource version so we can hook up a watch
-        return initial_pods.metadata.resource_version
+        return initial_resources.metadata.resource_version
 
     def _watch_and_update(self):
         """
-        Keeps the current list of pods up-to-date
+        Keeps the current list of resources up-to-date
 
         This method is to be run not on the main thread!
 
-        We first fetch the list of current pods, and store that. Then we
-        register to be notified of changes to those pods, and keep our
+        We first fetch the list of current resources, and store that. Then we
+        register to be notified of changes to those resources, and keep our
         local store up-to-date based on these notifications.
 
         We also perform exponential backoff, giving up after we hit 32s
@@ -95,32 +111,32 @@ class PodReflector(SingletonConfigurable):
         """
         cur_delay = 0.1
         while True:
-            self.log.info("watching for pods with label selector %s in namespace %s", self.label_selector, self.namespace)
+            self.log.info("watching for resources with label selector %s in namespace %s", self.label_selector, self.namespace)
             w = watch.Watch()
             try:
                 resource_version = self._list_and_update()
                 for ev in w.stream(
-                        self.api.list_namespaced_pod,
+                        getattr(self.api, self.list_method_name),
                         self.namespace,
                         label_selector=self.label_selector,
                         resource_version=resource_version,
                 ):
                     cur_delay = 0.1
-                    pod = ev['object']
+                    resource = ev['object']
                     if ev['type'] == 'DELETED':
                         # This is an atomic delete operation on the dictionary!
-                        self.pods.pop(pod.metadata.name, None)
+                        self.resources.pop(resource.metadata.name, None)
                     else:
                         # This is an atomic operation on the dictionary!
-                        self.pods[pod.metadata.name] = pod
+                        self.resources[resource.metadata.name] = resource
             except Exception:
                 cur_delay = cur_delay * 2
                 if cur_delay > 30:
-                    self.log.exception("Watching pods never recovered, giving up")
+                    self.log.exception("Watching resources never recovered, giving up")
                     if self.on_failure:
                         self.on_failure()
                     return
-                self.log.exception("Error when watching pods, retrying in %ss", cur_delay)
+                self.log.exception("Error when watching resources, retrying in %ss", cur_delay)
                 time.sleep(cur_delay)
                 continue
             finally:
@@ -130,17 +146,30 @@ class PodReflector(SingletonConfigurable):
         """
         Start the reflection process!
 
-        We'll do a blocking read of all pods first, so that we don't
+        We'll do a blocking read of all resources first, so that we don't
         race with any operations that are checking the state of the pod
         store - such as polls. This should be called only once at the
         start of program initialization (when the singleton is being created),
         and not afterwards!
         """
         if hasattr(self, 'watch_thread'):
-            raise ValueError('Thread watching for pods is already running')
+            raise ValueError('Thread watching for resources is already running')
 
         self._list_and_update()
         self.watch_thread = threading.Thread(target=self._watch_and_update)
         # If the watch_thread is only thread left alive, exit app
         self.watch_thread.daemon = True
         self.watch_thread.start()
+
+
+class PodReflector(NamespacedResourceReflector):
+    labels = {
+        'heritage': 'jupyterhub',
+        'component': 'singleuser-server',
+    }
+
+    list_method_name = 'list_namespaced_pod'
+
+    @property
+    def pods(self):
+        return self.resources
