f42e62005ea4cc3e71cf10dda8c0bace029014c5
yuvipanda <yuvipanda@gmail.com>
2017-07-25 09:11:53 -0700

    Add random jitter to the exponential backoff function

--- kubespawner/utils.py
+++ kubespawner/utils.py
@@ -2,6 +2,7 @@
 Misc. general utility functions, not tied to Kubespawner directly
 """
 from concurrent.futures import ThreadPoolExecutor
+import random
 
 from jupyterhub.utils import DT_MIN, DT_MAX, DT_SCALE
 from tornado import gen, ioloop
@@ -19,12 +20,20 @@ class SingletonExecutor(SingletonConfigurable, ThreadPoolExecutor):
 @gen.coroutine
 def exponential_backoff(func, fail_message, timeout=10, *args, **kwargs):
     loop = ioloop.IOLoop.current()
-    tic = loop.time()
+    start_tic = loop.time()
     dt = DT_MIN
-    while dt > 0:
+    while True:
+        if (loop.time() - start_tic) > timeout:
+            # We time out!
+            break
         if func(*args, **kwargs):
             return
         else:
             yield gen.sleep(dt)
-        dt = min(dt * DT_SCALE, DT_MAX, timeout - (loop.time() - tic))
+        # Add some random jitter to improve performance
+        # This makes sure that we don't overload any single iteration
+        # of the tornado loop with too many things
+        # See https://www.awsarchitectureblog.com/2015/03/backoff.html
+        # for a good example of why and how this helps
+        dt = min(DT_MAX, (1 + random.random()) * (dt * DT_SCALE))
     raise TimeoutError(fail_message)
