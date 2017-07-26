236823a63cb4733e05e507c0aab976d852be05d2
yuvipanda <yuvipanda@gmail.com>
2017-07-25 17:40:28 -0700

    Make exponential backoff algorithm self contained
    And change factors to 1.2 instead of 2, and use full Jitter

--- kubespawner/utils.py
+++ kubespawner/utils.py
@@ -4,7 +4,6 @@ Misc. general utility functions, not tied to Kubespawner directly
 from concurrent.futures import ThreadPoolExecutor
 import random
 
-from jupyterhub.utils import DT_MIN, DT_MAX, DT_SCALE
 from tornado import gen, ioloop
 from traitlets.config import SingletonConfigurable
 
@@ -18,7 +17,14 @@ class SingletonExecutor(SingletonConfigurable, ThreadPoolExecutor):
     pass
 
 @gen.coroutine
-def exponential_backoff(pass_func, fail_message, timeout=10, *args, **kwargs):
+def exponential_backoff(
+        pass_func,
+        fail_message,
+        start_wait=0.1,
+        scale_factor=1.2,
+        max_wait=5,
+        timeout=10,
+        *args, **kwargs):
     """
     Exponentially backoff until pass_func is true.
 
@@ -26,11 +32,15 @@ def exponential_backoff(pass_func, fail_message, timeout=10, *args, **kwargs):
     many iterations as needed, with maximum timeout timeout. If pass_func is
     still returning false at the end of timeout, a TimeoutError will be raised.
 
+    It'll start waiting at start_wait, scaling up by continuously multiplying itself
+    by scale_factor until pass_func returns true. It'll never wait for more than
+    max_wait seconds per iteration.
+
     *args and **kwargs are passed to pass_func.
     """
     loop = ioloop.IOLoop.current()
     start_tic = loop.time()
-    dt = DT_MIN
+    dt = start_wait
     while True:
         if (loop.time() - start_tic) > timeout:
             # We time out!
@@ -43,6 +53,7 @@ def exponential_backoff(pass_func, fail_message, timeout=10, *args, **kwargs):
         # This makes sure that we don't overload any single iteration
         # of the tornado loop with too many things
         # See https://www.awsarchitectureblog.com/2015/03/backoff.html
-        # for a good example of why and how this helps
-        dt = min(DT_MAX, (1 + random.random()) * (dt * DT_SCALE))
+        # for a good example of why and how this helps. We're using their
+        # full Jitter implementation equivalent.
+        dt = min(max_wait, random.uniform(0, dt * scale_factor))
     raise TimeoutError(fail_message)
