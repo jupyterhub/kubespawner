352c4683041be62aabdefcaec1ae2fecc31dbffc
yuvipanda <yuvipanda@gmail.com>
2017-08-29 16:35:23 -0400

    Add link to docs from README + bump version
    Version bump for removing 'jupyter' dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jupyterhub-kubespawner',
-    version='0.6.0',
+    version='0.6.1',
     install_requires=[
         'jupyterhub',
         'pyYAML',
