70dda06fd5bfc013dd77b5565d4dff52dc78c3f1
yuvipanda <yuvipanda@gmail.com>
2018-01-23 13:02:42 -0800

    Bump kubespawner version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jupyterhub-kubespawner',
-    version='0.7.1',
+    version='0.8',
     install_requires=[
         'jupyterhub>=0.8',
         'pyYAML',
