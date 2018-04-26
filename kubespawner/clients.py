f076ec713d801b05391451885be50f08adfe8e87
Min RK <benjaminrk@gmail.com>
2018-04-26 17:21:18 +0200

    monkeypatch kubernetes to avoid creating unused threadpools for each client
    kubernetes creates client objects in a number of places
    e.g. every time a Watch is instantiated.
    
    These objects create a max-size thread pool for async requests,
    which bogs down the process if enough client objects are instantiated.
    Since we don't use async requests, these pools go totally unused.
    
    api_client is monkeypatched to avoid creating these ThreadPools in the first place.
    A patch has been submitted upstream to swagger-codegen,
    which is responsible for creating the ThreadPool.

--- kubespawner/clients.py
+++ kubespawner/clients.py
@@ -1,6 +1,23 @@
+"""Shared clients for kubernetes
+
+avoids creating multiple kubernetes client objects,
+each of which spawns an unused max-size thread pool
+"""
+
+from unittest.mock import Mock
 import weakref
 
 import kubernetes.client
+from kubernetes.client import api_client
+
+# FIXME: remove when instantiating a kubernetes client
+# doesn't create N-CPUs threads unconditionally.
+# monkeypatch threadpool in kubernetes api_client
+# to avoid instantiating ThreadPools.
+# This is known to work for kubernetes-4.0
+# and may need updating with later kubernetes clients
+_dummy_pool = Mock()
+api_client.ThreadPool = lambda *args, **kwargs: _dummy_pool
 
 _client_cache = {}
 
