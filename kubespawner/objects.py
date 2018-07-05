359a588042e170df0e28f2a67404bcaf81c7942b
Erik Sundell <erik.i.sundell@gmail.com>
2018-07-05 11:01:46 +0200

    adds affinity api verification

--- kubespawner/objects.py
+++ kubespawner/objects.py
@@ -21,7 +21,7 @@ from kubernetes.client.models import (
     V1beta1IngressBackend,
     V1Toleration,
     V1Affinity,
-    V1NodeAffinity, V1NodeSelector, V1NodeSelectorTerm,
+    V1NodeAffinity, V1NodeSelector, V1NodeSelectorTerm, V1PreferredSchedulingTerm,
     V1PodAffinity, V1PodAntiAffinity, V1WeightedPodAffinityTerm, V1PodAffinityTerm,
 )
 
@@ -314,26 +314,46 @@ def make_pod(
         node_selector = None
         if node_affinity_required:
             node_selector = V1NodeSelector(
-                node_selector_terms=node_affinity_required,
+                node_selector_terms=[V1NodeSelectorTerm(**t) for t in node_affinity_required],
             )
 
+        preferred_scheduling_terms = None
+        if node_affinity_preferred:
+            preferred_scheduling_terms = [V1PreferredSchedulingTerm(**t) for t in node_affinity_preferred]
+
         node_affinity = V1NodeAffinity(
-            preferred_during_scheduling_ignored_during_execution=node_affinity_preferred,
+            preferred_during_scheduling_ignored_during_execution=preferred_scheduling_terms,
             required_during_scheduling_ignored_during_execution=node_selector,
         )
 
     pod_affinity = None
     if pod_affinity_preferred or pod_affinity_required:
+        weighted_pod_affinity_terms = None
+        if pod_affinity_preferred:
+            weighted_pod_affinity_terms = [V1WeightedPodAffinityTerm(**t) for t in pod_affinity_preferred]
+        
+        pod_affinity_terms = None
+        if pod_affinity_preferred:
+            pod_affinity_terms = [V1PodAffinityTerm(**t) for t in pod_affinity_required]
+
         pod_affinity = V1PodAffinity(
-            preferred_during_scheduling_ignored_during_execution=pod_affinity_preferred,
-            required_during_scheduling_ignored_during_execution=pod_affinity_required,
+            preferred_during_scheduling_ignored_during_execution=weighted_pod_affinity_terms,
+            required_during_scheduling_ignored_during_execution=pod_affinity_terms,
         )
 
     pod_anti_affinity = None
     if pod_anti_affinity_preferred or pod_anti_affinity_required:
-        pod_anti_affinity = V1PodAntiAffinity(
-            preferred_during_scheduling_ignored_during_execution=pod_anti_affinity_preferred,
-            required_during_scheduling_ignored_during_execution=pod_anti_affinity_required,
+        weighted_pod_affinity_terms = None
+        if pod_affinity_preferred:
+            weighted_pod_affinity_terms = [V1WeightedPodAffinityTerm(**t) for t in pod_anti_affinity_preferred]
+        
+        pod_affinity_terms = None
+        if pod_affinity_preferred:
+            pod_affinity_terms = [V1PodAffinityTerm(**t) for t in pod_anti_affinity_required]
+
+        pod_anti_affinity = V1PodAffinity(
+            preferred_during_scheduling_ignored_during_execution=weighted_pod_affinity_terms,
+            required_during_scheduling_ignored_during_execution=pod_affinity_terms,
         )
     
     affinity = None
