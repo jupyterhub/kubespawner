d65ab4a85e37f9b6ad061b6406f34c2e30577596
Erik Sundell <erik.i.sundell@gmail.com>
2018-08-01 23:02:19 +0200

    Conform to known naming conventions
    I have had a hard time grasping the ins and outs of traitlets, 
    recognizing the names used in jupyterhub_config.py helps so i simply 
    made them a bit more like that.

--- tests/test_spawner.py
+++ tests/test_spawner.py
@@ -10,16 +10,16 @@ def sync_wait(future):
     loop.run_until_complete(future)
     return future.result()
 
+
 def test_deprecated_config():
     """Deprecated config is handled correctly"""
-    cfg = Config()
-    ks_cfg = cfg.KubeSpawner
+    c = Config()
     # both set, non-deprecated wins
-    ks_cfg.singleuser_fs_gid = 5
-    ks_cfg.fs_gid = 10
+    c.KubeSpawner.singleuser_fs_gid = 5
+    c.KubeSpawner.fs_gid = 10
     # only deprecated set, should still work
-    ks_cfg.singleuser_extra_pod_config = extra_pod_config = {"key": "value"}
-    spawner = KubeSpawner(config=cfg, _mock=True)
+    c.KubeSpawner.singleuser_extra_pod_config = extra_pod_config = {"key": "value"}
+    spawner = KubeSpawner(config=c, _mock=True)
     assert spawner.fs_gid == 10
     assert spawner.extra_pod_config == extra_pod_config
     # deprecated access gets the right values, too
@@ -37,14 +37,14 @@ def test_deprecated_runtime_access():
     assert spawner.uid == 20
     assert spawner.singleuser_uid == 20
 
+
 def test_get_pod_manifest_tolerates_mixed_input():
     """
     Test that the get_pod_manifest function can handle a either a dictionary or
     an object both representing V1Container objects and that the function
     returns a V1Pod object containing V1Container objects.
     """
-    cfg = Config()
-    ks_cfg = cfg.KubeSpawner
+    c = Config()
 
     dict_model = {
         'name': 'mock_name_1',
@@ -61,9 +61,9 @@ def test_get_pod_manifest_tolerates_mixed_input():
             capabilities=V1Capabilities(add=['NET_ADMIN'])
         )
     )
-    ks_cfg.init_containers = [dict_model, object_model]
+    c.KubeSpawner.init_containers = [dict_model, object_model]
 
-    spawner = KubeSpawner(config=cfg, _mock=True)
+    spawner = KubeSpawner(config=c, _mock=True)
 
     # this test ensures the following line doesn't raise an error
     manifest = sync_wait(spawner.get_pod_manifest())
