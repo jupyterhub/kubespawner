41ca411db5bc855be422f4fbe86360da8befb0a2
Erik Sundell <erik.i.sundell@gmail.com>
2018-08-10 18:30:33 +0200

    Tests: test make_pod's output through spawner's get_pod_manifest method

--- tests/test_spawner.py
+++ tests/test_spawner.py
@@ -3,8 +3,16 @@ from unittest.mock import Mock
 from jupyterhub.objects import Hub, Server
 import pytest
 from traitlets.config import Config
-
+from asyncio import get_event_loop
 from kubespawner import KubeSpawner
+from kubernetes.client.models import (
+    V1SecurityContext, V1Container, V1Capabilities, V1Pod
+)
+
+def sync_wait(future):
+    loop = get_event_loop()
+    loop.run_until_complete(future)
+    return future.result()
 
 
 class MockUser(Mock):
@@ -94,3 +102,39 @@ async def test_spawn_progress(kube_ns, kube_client, config):
     await start_future
     # stop the pod
     await spawner.stop()
+
+
+def test_get_pod_manifest_tolerates_mixed_input():
+    """
+    Test that the get_pod_manifest function can handle a either a dictionary or
+    an object both representing V1Container objects and that the function
+    returns a V1Pod object containing V1Container objects.
+    """
+    c = Config()
+
+    dict_model = {
+        'name': 'mock_name_1',
+        'image': 'mock_image_1',
+        'command': ['mock_command_1']
+    }
+    object_model = V1Container(
+        name="mock_name_2",
+        image="mock_image_2",
+        command=['mock_command_2'],
+        security_context=V1SecurityContext(
+            privileged=True,
+            run_as_user=0,
+            capabilities=V1Capabilities(add=['NET_ADMIN'])
+        )
+    )
+    c.KubeSpawner.init_containers = [dict_model, object_model]
+
+    spawner = KubeSpawner(config=c, _mock=True)
+
+    # this test ensures the following line doesn't raise an error
+    manifest = sync_wait(spawner.get_pod_manifest())
+
+    # and tests the return value's types
+    assert isinstance(manifest, V1Pod)
+    assert isinstance(manifest.spec.init_containers[0], V1Container)
+    assert isinstance(manifest.spec.init_containers[1], V1Container)
