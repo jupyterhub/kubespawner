f002f9b9578fe80c5876831dbb42996312c12cc6
Min RK <benjaminrk@gmail.com>
2018-12-11 13:56:57 +0100

    release 0.10.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if v[:2] < (3, 5):
 
 setup(
     name='jupyterhub-kubespawner',
-    version='0.10.0',
+    version='0.10.1',
     install_requires=[
         'jupyterhub>=0.8',
         'pyYAML',
