fd0e1b5346a3b8a3741f47a1e392a6ccdd5c6e98
Yuvi Panda <yuvipanda@gmail.com>
2019-01-28 09:17:39 -0800

    Ping kubernetes version to 8.0
    9.0 alpha has breaking changes, and our version selector
    was including 9.0alpha.
    
    Breaking changes listed here:
    
    https://github.com/kubernetes-client/python/blob/master/CHANGELOG.md#v900a1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=[
         'jupyterhub>=0.8',
         'pyYAML',
-        'kubernetes>=7',
+        'kubernetes==7.0.0',
         'escapism',
         'jinja2',
         'async_generator>=1.8',
