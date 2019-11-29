1503ecf1d5569e6087233d8abf6619c78ac698ff
Erik Sundell <erik.i.sundell@gmail.com>
2019-11-29 22:53:29 +0100

    Bump version: 0.11.1 → 0.11.2.dev

--- kubespawner/spawner.py
+++ kubespawner/spawner.py
@@ -45,7 +45,7 @@ from kubespawner.reflector import NamespacedResourceReflector
 from asyncio import sleep
 from async_generator import async_generator, yield_
 
-__version__ = '0.11.1'
+__version__ = '0.11.2.dev'
 
 class PodReflector(NamespacedResourceReflector):
     """
