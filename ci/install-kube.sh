de57a6123a8e52dcb7290420ddeacb68be1296b1
Erik Sundell <erik.i.sundell@gmail.com>
2020-11-15 23:53:38 +0100

    Migrate from travis to GitHub actions

--- ci/install-kube.sh
+++ ci/install-kube.sh
@@ -7,21 +7,23 @@ mkdir -p bin $HOME/.kube $HOME/.minikube
 touch $KUBECONFIG
 
 # install kubectl, minikube
-# based on https://github.com/LiliC/travis-minikube
 echo "installing kubectl"
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v${KUBE_VERSION}/bin/linux/amd64/kubectl
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v${K8S_VERSION}/bin/linux/amd64/kubectl
 chmod +x kubectl
 mv kubectl bin/
 
 echo "installing minikube"
+if [[ "${K8S_VERSION:2:2}" -ge "16" ]]; then
+    echo "installing conntrack, a dependency for minikube"
+    sudo apt-get -q -y install conntrack
+fi
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v${MINIKUBE_VERSION}/minikube-linux-amd64
 chmod +x minikube
 mv minikube bin/
 
 echo "starting minikube"
-sudo $PWD/bin/minikube start --vm-driver=none --kubernetes-version=v${KUBE_VERSION}
-sudo chown -R travis: /home/travis/.minikube/
-minikube update-context
+sudo $PWD/bin/minikube start --vm-driver=none --kubernetes-version=v${K8S_VERSION}
+#FIXME? sudo chown -R travis: /home/travis/.minikube/
 
 
 # can be used to check a condition of nodes and pods
