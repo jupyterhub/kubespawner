571507f74a26711c74e3cdc77778008d27ed0a44
Erik Sundell <erik.i.sundell@gmail.com>
2021-07-21 13:25:57 +0200

    Bump version: 1.0.1.dev → 1.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if v[:2] < (3, 6):
 
 setup(
     name='jupyterhub-kubespawner',
-    version='1.0.1.dev',
+    version='1.1.0',
     install_requires=[
         'async_generator>=1.8',
         'escapism',
