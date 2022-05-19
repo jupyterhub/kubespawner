616f72c4aee26c3d2127c6af6086ec50d6cda383
Georgiana Dolocan <georgiana.dolocan@gmail.com>
2022-05-19 20:01:58 +0300

    Bump version: 4.1.0 → 4.1.1.dev

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if v[:2] < (3, 7):
 
 setup(
     name='jupyterhub-kubespawner',
-    version='4.1.0',
+    version='4.1.1.dev',
     install_requires=[
         'escapism',
         'python-slugify',
