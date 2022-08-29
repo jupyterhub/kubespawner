7d6d82c2be469dd76f770d6f6ed0d1dade6b24a7
Erik Sundell <erik@sundellopensource.se>
2022-08-29 12:41:41 +0200

    Bump version: 4.2.0 → 4.2.1.dev

--- kubespawner/__init__.py
+++ kubespawner/__init__.py
@@ -12,5 +12,5 @@ in your `jupyterhub_config.py` file.
 # instead of the more verbose import kubespawner.spawner.KubeSpawner.
 from .spawner import KubeSpawner
 
-__version__ = '4.2.0'
+__version__ = '4.2.1.dev'
 __all__ = ["KubeSpawner"]
