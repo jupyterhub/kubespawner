bd154773136329356265ea10f5f10c0333880fc8
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-25 01:30:23 +0000

    Adding tarfile member sanitization to extractall()

--- tests/jupyterhub_config.py
+++ tests/jupyterhub_config.py
@@ -29,7 +29,26 @@ if os.path.exists(ssl_tar_file):
     c.JupyterHub.internal_certs_location = ssl_dir
 
     with tarfile.open(ssl_tar_file) as tf:
-        tf.extractall(path="/etc/jupyterhub")
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tf, path="/etc/jupyterhub")
 
     for root, dirs, files in os.walk("/etc/jupyterhub"):
         for name in files:
