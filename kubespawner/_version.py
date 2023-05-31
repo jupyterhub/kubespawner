bd460b7efecfe26290560cbccdddb121c119fac6
Erik Sundell <erik.i.sundell@gmail.com>
2023-05-31 11:10:29 +0200

    Bump to 6.0.0

--- kubespawner/_version.py
+++ kubespawner/_version.py
@@ -1,4 +1,4 @@
 # __version__ should be updated using tbump, based on configuration in
 # pyproject.toml, according to instructions in RELEASE.md.
 #
-__version__ = "5.0.1.dev0"
+__version__ = "6.0.0"
