49de7eafacc85c753fc67f45a6457087060357e9
Erik Sundell <erik.i.sundell@gmail.com>
2024-09-22 17:18:36 +0200

    Add KubeSpawner.version_info

--- kubespawner/_version.py
+++ kubespawner/_version.py
@@ -2,3 +2,6 @@
 # pyproject.toml, according to instructions in RELEASE.md.
 #
 __version__ = "6.2.1.dev"
+
+# version_info looks like (1, 2, 3, "dev") if __version__ is 1.2.3.dev
+version_info = tuple(int(p) if p.isdigit() else p for p in __version__.split("."))
