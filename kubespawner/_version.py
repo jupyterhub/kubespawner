621be0e68d9dea38ac899c6054f966dd68d1d028
Min RK <benjaminrk@gmail.com>
2024-10-02 12:45:42 +0200

    Bump to 7.0.0b2

--- kubespawner/_version.py
+++ kubespawner/_version.py
@@ -1,7 +1,7 @@
 # __version__ should be updated using tbump, based on configuration in
 # pyproject.toml, according to instructions in RELEASE.md.
 #
-__version__ = "7.0.0b1"
+__version__ = "7.0.0b2"
 
 # version_info looks like (1, 2, 3, "dev") if __version__ is 1.2.3.dev
 version_info = tuple(int(p) if p.isdigit() else p for p in __version__.split("."))
