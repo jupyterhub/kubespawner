263bf894e368f5fb026835a2b23a00426e82b1d7
Simon Li <orpheus+devel@gmail.com>
2024-10-14 14:57:53 +0100

    CI: Throw Exception if namespace not deleted after 20s

--- tests/conftest.py
+++ tests/conftest.py
@@ -249,6 +249,7 @@ async def _delete_namespace(client, namespace):
         else:
             print("waiting for %s to delete" % namespace)
             await asyncio.sleep(1)
+    raise Exception(f"Namespace {namespace} not deleted after 20 s")
 
 
 @pytest_asyncio.fixture(scope="session")
