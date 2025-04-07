72af3cc10ff5ddd601bc64999189096e37eb8b50
YuviPanda <yuvipanda@gmail.com>
2025-04-07 13:56:05 -0700

    Put servername only on pod

--- kubespawner/spawner.py
+++ kubespawner/spawner.py
@@ -2245,7 +2245,6 @@ class KubeSpawner(Spawner):
         # Annotations don't need to be escaped
         annotations = {
             'hub.jupyter.org/username': self.user.name,
-            'hub.jupyter.org/servername': self.name,
             "hub.jupyter.org/kubespawner-version": __version__,
             "hub.jupyter.org/jupyterhub-version": jupyterhub.__version__
         }
@@ -2378,6 +2377,8 @@ class KubeSpawner(Spawner):
         annotations = self._build_common_annotations(
             self._expand_all(self.extra_annotations)
         )
+        # Unconditionally put servername on the pods
+        annotations['hub.jupyter.org/servername'] = self.name
 
         return make_pod(
             name=self.pod_name,
